d7e5e8305ef28b6797ecd71b44a1a489d7326991
Mac <Mac@Macs-Air.itpro.tv>
2017-03-15 19:43:17 -0400

    added angular to meantodo application

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
-console.log('Working....');
+(function(){
+  angular.module('MEANTodos', [])
+   .controller('TodoController', TodoController);
+
+   TodoController.$inject = ['$scope'];
+
+   function TodoController($scope){
+     $scope.message = 'I work';
+   }
+})()   //immediately invoked functional expression
