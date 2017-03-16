0f25704951fb8738fc4144d950cbeb6fd24d1c97
sam gustafson <samelgw@gmail.com>
2017-03-15 20:46:18 -0400

    refactor todoController into separate and included that file in index.html

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,4 @@
 (function(){
-  angular.module('MEANTodos', [])
-   .controller('TodoController', TodoController);
+  angular.module('MEANTodos', []); // setter syntax
 
-   TodoController.$inject = ['$scope'];
-
-   function TodoController($scope){
-     $scope.message = 'I work';
-   }
-})()   //immediately invoked functional expression
+   })()   //immediately invoked functional expression
