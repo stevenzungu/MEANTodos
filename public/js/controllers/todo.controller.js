864b1412fd64de3e15c1925f25550b6d531517bf
Justin Dennison <justin1dennison@gmail.com>
2017-03-17 19:41:38 -0400

    implemented the ability for a user to create a new todo using a form. Added create method to the TodoService.

--- public/js/controllers/todo.controller.js
+++ public/js/controllers/todo.controller.js
@@ -6,8 +6,17 @@
 
     function TodoController($scope, TodoService){
       $scope.todos = [];
+      $scope.newTodo = {};
       $scope.getTodos = getTodos;
+      $scope.addTodo = addTodo;
 
+      function addTodo(newTodo){
+        console.log('Creating a new todo...');
+        TodoService.create(newTodo)
+                  .then(function(response){
+                    getTodos();
+                  });
+      }
       function getTodos(){
         console.log('Getting the todos...');
         TodoService.getAll()
