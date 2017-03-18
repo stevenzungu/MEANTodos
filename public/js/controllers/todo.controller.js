2e6978dd5e082c3d5bc9bc1058f96efb5d3f1cdb
Mac <Mac@Macs-Air.itpro.tv>
2017-03-17 20:11:12 -0400

    removed the get todos button and instead make that happen on pageload. Also cleared the form on page load.

--- public/js/controllers/todo.controller.js
+++ public/js/controllers/todo.controller.js
@@ -6,11 +6,10 @@
 
   function TodoController($scope, TodoService){
     $scope.todos = [];
-
     $scope.newTodo = {};
-    $scope.getTodos = getTodos;
     $scope.addTodo = addTodo;
     $scope.deleteTodo = deleteTodo;
+    getTodos();
 
     function deleteTodo(todo){
       TodoService.delete(todo)
@@ -24,6 +23,7 @@
       TodoService.create(newTodo)
                   .then(function(response){
                     getTodos();
+                    $scope.newTodo = {};
                   });
     }
     function getTodos(){
